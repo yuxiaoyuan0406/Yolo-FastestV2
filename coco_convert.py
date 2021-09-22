e26c9c40e974e8b1e7802b9ad8f4a48282e499fc
yuxiaoyuan0406 <973971990@qq.com>
2021-09-22 22:18:02 +0800

    finished

--- coco_convert.py
+++ coco_convert.py
@@ -8,7 +8,7 @@ import cv2 as cv
 import os
 
 dataDir = os.environ['COCO2017']
-dataType = 'train2017'
+dataType = 'val2017'
 annFile = os.path.join(dataDir, 'annotations', 'instances_{}.json'.format(dataType))
 
 coco = COCO(annFile)
@@ -42,6 +42,19 @@ for i in range(len(imgs)):
     img = imgs[i]
     file_name = os.path.join(dataDir, dataType, imgs[i]['file_name'])
     dst_file = os.path.join(dst_dir, '{:0>5d}.jpg'.format(i+1))
+
+    # get annotations
+    annIds = coco.getAnnIds(imgIds=imgIds[i], iscrowd=None)
+    anns = coco.loadAnns(annIds)
+
     # print(copy_cmd.format(file_name, dst_file))
     os.system(copy_cmd.format(file_name, dst_file))
-    
+    txt = open(os.path.join(dst_dir, '{:0>5d}.txt'.format(i+1)), 'w+')
+    for j in range(len(anns)):
+        x, y, w, h = anns[j]['bbox']
+        # print(x, y, w, h)
+        if anns[j]["category_id"] in [1, 2, 4]:
+            txt.write('{:d} {:f} {:f} {:f} {:f}\n'.format(anns[j]["category_id"], (x+w/2)/img['width'], (y+h/2)/img['height'], w/img['width'], h/img['height']))
+        
+    # json.write(str(imgs[i]))
+    txt.close()
