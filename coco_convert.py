ba2d5de018eee51c38c19c71c5f7d197d06a2f71
yuxiaoyuan0406 <yuxiaoyuan0406@qq.com>
2021-09-22 16:17:53 +0800

    make a coco dateset converter

---
+++ coco_convert.py
@@ -0,0 +1,40 @@
+from pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+import cv2 as cv
+
+dataDir = '/home/vip/Dataset/coco/2017'
+dataType = 'train2017'
+annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
+
+coco = COCO(annFile)
+
+# display COCO categories and supercategories
+cats = coco.loadCats(coco.getCatIds())
+nms=[cat['name'] for cat in cats]
+print('COCO categories: \n{}\n'.format(' '.join(nms)))
+
+nms = set([cat['supercategory'] for cat in cats])
+print('COCO supercategories: \n{}'.format(' '.join(nms)))
+
+# get all images containing given categories, select one at random
+catIds = coco.getCatIds(catNms=['bicycle','car','motorcycle'])
+imgIds = coco.getImgIds(catIds=catIds )
+# imgIds = coco.getImgIds(imgIds = [55809])
+img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
+
+image_name = img['file_name']
+
+# load and display image
+I = io.imread('%s/%s/%s'%(dataDir,dataType,img['file_name']))
+# use url to load image
+# I = io.imread(img['coco_url'])
+plt.imshow(I)
+plt.axis('off')
+# plt.show()
+annIds = coco.getAnnIds(imgIds=img['id'], catIds=[], iscrowd=None)
+anns = coco.loadAnns(annIds)
+coco.showAnns(anns, True)
+plt.show()
