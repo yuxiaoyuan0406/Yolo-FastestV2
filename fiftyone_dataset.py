041c55c2d4585b4d5ca147d6a17d45a17c4b310e
yuxiaoyuan0406 <973971990@qq.com>
2021-10-04 17:26:04 +0800

    try to use fiftyone dataset

---
+++ fiftyone_dataset.py
@@ -0,0 +1,12 @@
+import fiftyone as fo
+
+if __name__ == '__main__':
+    dataset = fo.zoo.load_zoo_dataset('open-images-v6', 
+        split='train', 
+        label_types=['detections'], 
+        classes=['Bicycle', 'Motorcycle'],
+        max_samples=500)
+    session = fo.launch_app(dataset=dataset, port=5151)
+
+    while True:
+        pass
