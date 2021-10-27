75a6c62227726c25d8c8237f6f14bcc7dee3d3b8
yuxiuaoyuan0406 <yuxiaoyuan0406@hotmail.com>
2021-10-27 14:57:33 +0800

    make re-use-able

--- fiftyone_dataset.py
+++ fiftyone_dataset.py
@@ -1,7 +1,7 @@
 import fiftyone as fo
 
-if __name__ == '__main__':
-    dataset = fo.zoo.load_zoo_dataset(
+def make_dataset():
+    return fo.zoo.load_zoo_dataset(
         name='open-images-v6',
         split='train',
         label_types=['detections'],
@@ -9,7 +9,10 @@ if __name__ == '__main__':
         max_samples=50000
     )
 
-    '''
+
+if __name__ == '__main__':
+    dataset = make_dataset()
+    # '''
     session = fo.launch_app(dataset=dataset, port=5151)
 
     while True:
@@ -20,10 +23,10 @@ if __name__ == '__main__':
             session.close()
             # dataset.delete()
             break
-    '''
+    # '''
 
-    export_dir = '/media/dennis/DENNIS/fiftyone/open-images-v6/yolov4'
-    dataset_type = fo.types.YOLOv4Dataset
+    # export_dir = '/media/dennis/DENNIS/fiftyone/open-images-v6/yolov4'
+    # dataset_type = fo.types.YOLOv4Dataset
 
-    dataset.export(export_dir, dataset_type)
+    # dataset.export(export_dir, dataset_type)
     dataset.delete()
