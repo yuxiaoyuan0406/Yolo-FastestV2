baa0710fe12a5af4a34a93c5cbf768c0193b97c4
yuxiaoyuan0406 <973971990@qq.com>
2021-11-12 14:35:55 +0800

    add a new file

---
+++ rtsp_server.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+
+import cv2
+import gi
+
+gi.require_version('Gst', '1.0')
+gi.require_version('GstRtspServer', '1.0')
+from gi.repository import Gst, GstRtspServer, GObject
+
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=59,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+# see: https://stackoverflow.com/questions/47396372/write-opencv-frames-into-gstreamer-rtsp-server-pipeline?rq=1
+class SensorFactory(GstRtspServer.RTSPMediaFactory):
+    def __init__(self, **properties):
+        super(SensorFactory, self).__init__(**properties)
+        self.cap = cv2.VideoCapture(gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+        self.number_frames = 0
+        self.fps = 30
+        self.duration = 1 / self.fps * Gst.SECOND  # duration of a frame in nanoseconds
+        self.launch_string = 'appsrc name=source is-live=true block=true format=GST_FORMAT_TIME ' \
+                             'caps=video/x-raw,format=BGR,width=640,height=480,framerate={}/1 ' \
+                             '! videoconvert ! video/x-raw,format=I420 ' \
+                             '! x264enc speed-preset=ultrafast tune=zerolatency ' \
+                             '! rtph264pay config-interval=1 name=pay0 pt=96'.format(self.fps)
+
+    def on_need_data(self, src, lenght):
+        if self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if ret:
+                data = frame.tostring()
+                buf = Gst.Buffer.new_allocate(None, len(data), None)
+                buf.fill(0, data)
+                buf.duration = self.duration
+                timestamp = self.number_frames * self.duration
+                buf.pts = buf.dts = int(timestamp)
+                buf.offset = timestamp
+                self.number_frames += 1
+                retval = src.emit('push-buffer', buf)
+                print('pushed buffer, frame {}, duration {} ns, durations {} s'.format(self.number_frames,
+                                                                                       self.duration,
+                                                                                       self.duration / Gst.SECOND))
+                if retval != Gst.FlowReturn.OK:
+                    print(retval)
+
+    def do_create_element(self, url):
+        return Gst.parse_launch(self.launch_string)
+
+    def do_configure(self, rtsp_media):
+        self.number_frames = 0
+        appsrc = rtsp_media.get_element().get_child_by_name('source')
+        appsrc.connect('need-data', self.on_need_data)
+
+
+class GstServer(GstRtspServer.RTSPServer):
+    def __init__(self, **properties):
+        super(GstServer, self).__init__(**properties)
+        self.factory = SensorFactory()
+        self.factory.set_shared(True)
+        self.get_mount_points().add_factory("/test", self.factory)
+        self.attach(None)
+
+if __name__ == '__main__':
+    GObject.threads_init()
+    Gst.init(None)
+
+    server = GstServer()
+
+    loop = GObject.MainLoop()
+    loop.run()
